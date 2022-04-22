54f74f54d5e61795704c7cbd75fefa285939eb9c
Rod Fuentes <joserodrigofuentes@gmail.com>
2022-04-22 17:27:31 -0600

    updated winner modal to show ROI concept

--- packages/react-app/src/components/SampleWinningModal.jsx
+++ packages/react-app/src/components/SampleWinningModal.jsx
@@ -22,17 +22,23 @@ const SampleWinningModal = props => {
         </Button>
         <Modal className="winner-modal" title="Latest Bids" visible={showBidHistoryModal} onOk={handleOk} onCancel={handleCancel}>
             <Row justify="center" style={{ marginTop: 24, marginBottom: 24 }}>
-                <Col span={24} align="middle">
+                <Col span={18} align="middle">
                     <h1>Congratulations!</h1>
-                    <h5>You outbid the competition and</h5>
-                        <h5>earned 20 points for bidding.
-                        <Tooltip placement="right" title="You earn rewards every time you bid, 
-                        where the number of points is based on the number of hours 
+                    <h5>You earned 20 points by bidding.
+                        <Tooltip placement="right" title="Get rewards every time you bid, 
+                        where the amount is based on the number of hours 
                         remaining in the auction."> <i className="bi bi-info-circle bid-info winner-info-icon"></i></Tooltip>
                     </h5>
-                    <h1><img src={rewardsImage} style={{height: 50}} /></h1>
-                    <h5>Your Rewards Balance </h5>
-                    <h5>{rewards && ethers.utils.commify(rewards)}</h5>
+                    <img src={rewardsImage} className="winner-modal-badge"/>
+                    <h5>You will win this NFT if you're not outbid within 24 hours!</h5>
+                    <p>
+                        You bid Ξ0.0021. If you win this NFT – worth approximately 
+                        Ξ124 ($334,234) – you can flip it for Ξ123.99 in profit.
+                    </p>                    
+                    <h6>That's a 104,122x ROI! 🤑</h6>
+
+                    {/* <h5>Your Rewards Balance </h5>
+                    <h5>{rewards && ethers.utils.commify(rewards)}</h5> */}
                 </Col>
             </Row>
         </Modal>
