f277f54aeffd201adb069a68ef3c4621f15dac3e
Rod Fuentes <joserodrigofuentes@gmail.com>
2022-05-07 15:57:59 -0600

    cleaned up Tx Pending Modal

--- packages/react-app/src/components/TransactionPendingModal.jsx
+++ packages/react-app/src/components/TransactionPendingModal.jsx
@@ -1,60 +1,15 @@
 import { Col, Modal, Row, Button } from "antd";
-import rewardsImage from "../img/rewards.png";
-import { displayWeiAsEther } from "../helpers";
-import { ethers } from "ethers";
 import { useContext } from "react";
 import AuctionOptionsContext from "../contexts/AuctionOptionsContext";
 
 const TransactionPendingModal = props => {
   const { showTransactionModal, setShowTransactionModal } = props;
-  const { price } = props;
-  const auctionOptions = useContext(AuctionOptionsContext);
 
   const handleOk = evt => {
     setShowTransactionModal(false);
   };
   const handleCancel = evt => {
     setShowTransactionModal(false);
-  };
-
-  const nftPriceInUsd = () => {
-    if (auctionOptions.stats.floor_price && price) {
-      return ethers.utils.commify(
-        parseFloat(auctionOptions.stats.floor_price * price).toFixed(2),
-      );
-    } else {
-      return "0";
-    }
-  };
-
-  const nftEthProfit = () => {
-    if (auctionOptions.stats.floor_price && auctionOptions.maxBid) {
-      const floor_price = ethers.utils.parseEther(
-        auctionOptions.stats.floor_price + "",
-      ); // standardizing opensea value to big number holding wei
-      const maxBid = auctionOptions.maxBid;
-      return displayWeiAsEther(floor_price.sub(maxBid));
-    } else {
-      return "0";
-    }
-  };
-
-  const roi = () => {
-    if (
-      auctionOptions.maxBid &&
-      auctionOptions.maxBid.gt(0) &&
-      auctionOptions.stats.floor_price
-    ) {
-      const floor_price = ethers.utils.parseEther(
-        auctionOptions.stats.floor_price + "",
-      ); // standardizing opensea value to big number holding wei
-      const maxBid = auctionOptions.maxBid;
-      let ans = floor_price.sub(maxBid).div(maxBid);
-      ans = ethers.utils.commify(ans);
-      return ans;
-    } else {
-      return "Gazillion";
-    }
   };
 
   return (
