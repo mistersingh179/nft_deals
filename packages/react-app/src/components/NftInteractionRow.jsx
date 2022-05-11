2d77bc19040f687669405f700fd3885e4ac5b97e
Rod Fuentes <joserodrigofuentes@gmail.com>
2022-05-11 11:39:35 -0600

    updated to remove collection floor price and replace with Trait Sniper details

--- packages/react-app/src/components/NftInteractionRow.jsx
+++ packages/react-app/src/components/NftInteractionRow.jsx
@@ -47,15 +47,17 @@ const NftInteractionRow = props => {
           </Tooltip>
         </h1>
         <h2>
-          Collection Floor Price: Ξ {auctionOptions.stats.floor_price}
+          {/* Collection Floor Price: Ξ {auctionOptions.stats.floor_price} */}
+          Trait Sniper <a href="https://app.traitsniper.com/bff-you?token=1785&view=1785" target="_blank" className="trait-details">Rarity Rank #237</a> of 10,000
           <span className="smaller-usdc">
-            (~
+            <p>Key Traits: Ice Cream Cone (0.33%) and Angel Wings (0.3%)</p>
+            {/* (~
             {auctionOptions.stats.floor_price &&
               price &&
               `$${ethers.utils.commify(
                 (auctionOptions.stats.floor_price * price).toFixed(2),
               )}`}
-            )
+            ) */}
           </span>
         </h2>
         <div className="row">
