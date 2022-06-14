ccbb2850db222226782575543df2729b6964e542
Mister Singh <mistersingh179@gmail.com>
2022-06-13 21:03:52 -0400

    added subgraph to track auctions & bids
    added new yarn commands to work with graph on every chain
    added mapping to build auctions & bids in the graph
    added Auction & Bid entities for the graph
    added subgraph templates for each network

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -3,7 +3,10 @@ import {
   YourContract,
   SetPurpose,
 } from "../generated/YourContract/YourContract";
-import { Purpose, Sender } from "../generated/schema";
+import {Purpose, Sender, Auction, Bid} from "../generated/schema";
+import {AuctionGenerated} from "../generated/AuctionFactory/AuctionFactory";
+import {Bid as BidGenerated} from "../generated/templates/Auction/Auction";
+import {Auction as AuctionTemplate} from "../generated/templates";
 
 export function handleSetPurpose(event: SetPurpose): void {
   let senderString = event.params.sender.toHexString();
@@ -30,4 +33,32 @@ export function handleSetPurpose(event: SetPurpose): void {
 
   purpose.save();
   sender.save();
+}
+
+export function handleAuctionGenerated(event: AuctionGenerated): void {
+  let auction = Auction.load(event.params.auctionContractAddress.toHex())
+  if(auction == null) {
+    const auction = new Auction(event.params.auctionContractAddress.toHex());
+    auction.createdAt = event.block.timestamp;
+    auction.listerAddress = event.params.nftOwner;
+    auction.contractAddress = event.params.auctionContractAddress;
+    auction.bidsCount =  BigInt.fromI32(0);
+    auction.save();
+    AuctionTemplate.create(event.params.auctionContractAddress)
+  }
+}
+
+export function handleBidGenerated(event: BidGenerated): void {
+  let auction = Auction.load(event.address.toHexString())
+  if(auction !== null){
+    auction.bidsCount = auction.bidsCount.plus(BigInt.fromI32(1));
+    auction.save();
+  }
+
+  const bid = new Bid(event.transaction.hash.toHex());
+  bid.fromAddress = event.params.from;
+  bid.amount = event.params.amount;
+  bid.auction = event.address.toHexString();
+  bid.createdAt = event.block.timestamp;
+  bid.save();
 }
